ac519ec03634027ce1a576bcc2b4fd5a379a73d3
DarkJediDJ <xintut@mail.ru>
2021-11-07 14:34:51 +0300

    added deposit function

---
+++ deposit.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"errors"
+	"sync"
+)
+
+// Deposit provides way to add value to your btcwallet
+func (bw *BtcWallet) Deposit(value float64, wg *sync.WaitGroup) error {
+	bw.Lock()
+	defer bw.Unlock()
+	if value <= 0 {
+		return errors.New("Youre trying to deposit negative or zero value")
+	}
+	bw.btc += value
+	return nil
+}
