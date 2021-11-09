3bd5235bb11d1bdcbc46e2564f38a86614c2ed99
DarkJediDJ <xintut@mail.ru>
2021-11-09 18:45:50 +0300

    naming fix

--- withdraw.go
+++ withdraw.go
@@ -1,4 +1,4 @@
-package main
+package btcwallet
 
 import (
 	"errors"
@@ -12,7 +12,7 @@ func (bw *BtcWallet) Withdraw(value float64, wg *sync.WaitGroup) error {
 	if value <= 0 {
 		return errors.New("Youre trying to deposit negative or zero value")
 	}
-	if bw.btc-value < 0 {
+	if bw.btc < value {
 		return errors.New("You cant withdraw more mouney then you have")
 	}
 	bw.btc -= value
